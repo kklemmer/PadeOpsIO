24655c0bec10ea49d814f4b3387d5b17e08f0f1d
Kirby <kirby.heck@gmail.com>
2022-03-27 16:19:01 -0400

    resolved conflicts in testing.py

--- padeopsIO/testing.py
+++ padeopsIO/testing.py
@@ -4,13 +4,22 @@ import numpy as np
 
 # sys.path.append(r'./padeops_io/')  # why is this our cwd? 
 from padeopsIO import BudgetIO
+from inflow import InflowParser
+from padeplots import PlotsIO
 
 # testIO = BudgetIO('some_dir', verbose=False)
-testIO = BudgetIO(r'./test_data', Lx=1, Ly=1, Lz=1, runid=1, verbose=True)
+testIO = BudgetIO(r'./test_data', verbose=True)
+
+PlotsIO.xy_slice(testIO, ['ubar', 'uwake'], z=5)
 
 budgets = testIO.existing_budgets()
 testIO.existing_terms(budgets[0])
-testIO.read_budgets(budget_terms='all')
-testIO.write_npz(budget_terms='all')
+testIO.read_budgets(budget_terms=['ubar'])
+testIO.read_budgets(budget_terms=['ubar', 'vbar', 'wbar', (0, 1), '(0, 1)', 'pbar'])
+testIO.calc_wake()
+# testIO.write_npz(budget_terms='all', filename='overwrite_test', overwrite=True)
+
+InflowParser.inflow_budgets(testIO)
+
 
 print(testIO.__dict__)
