274830234e099b1e0c655c2e0adbf28185aef0ae
KirbyH <tg877441@login1.stampede2.tacc.utexas.edu>
2022-07-25 13:52:41 -0500

    Added file to store turbine information

---
+++ padeopsIO/turbineArray.py
@@ -0,0 +1,90 @@
+import numpy as np
+import os
+import re
+import warnings
+import glob
+
+try: 
+    import f90nml
+except ImportError: 
+    warnings.warn("Could not find package f90nml in system path. ")
+    # TODO - handle this somehow
+    f90nml = None
+
+import padeopsIO.budgetkey as budgetkey  # defines key pairing
+import padeopsIO.inflow as inflow  # interface to retrieve inflow profiles
+
+class TurbineArray(): 
+    """
+    # TODO fix class docstring
+    """
+    
+    def __init__(self, turb_dir, num_turbines=None, ADM_type=2, verbose=False): 
+        """
+        Constructor function for a TurbineArray class
+        
+        Parameters
+        ----------
+        turb_dir (path-like) : path to a turbine array directory in PadeOps
+        num_turbines (int) : optional, number of turbines. Default: number 
+            of turbine files in turbine directory
+        ADM_type (int) : ADM type. Default: 2. 
+        verbose (bool) : additional print statements
+        
+        Returns
+        -------
+        TurbineArray class instance
+        """
+        
+        self.turb_dir = turb_dir
+        self.ADM_type = ADM_type
+        self.verbose = verbose
+        
+        # begin reading in turbines
+        filenames = os.listdir(turb_dir)
+        
+        if num_turbines is not None: 
+            self.num_turbines = num_turbines
+            
+            if num_turbines != len(filenames):  
+                warnings.warn("Not all turbines in the specified directory will be used")
+
+                if self.verbose: 
+                    print("\tRequested {:d} turbines, but found {:d} files.".format(num_turbines, len(filenames)))
+        else: 
+            self.num_turbines = len(filenames)
+            
+        
+        # for now, each turbine can simply be a dictionary appended to a list
+        self.array = []
+        if self.verbose: 
+            print("Reading turbines from the following files:\n", filenames)
+            
+        for i, filename in enumerate(filenames): 
+            if i >= self.num_turbines: 
+                break  # only read in up to num_turbines turbines! 
+                
+            turb_nml = f90nml.read(os.path.join(turb_dir, filename))
+            self.array.append(turb_nml)
+            
+            if self.verbose: 
+                print("\tTurbineArray: added turbine to array form file", filename)
+        
+        if self.num_turbines == 1: 
+            if self.verbose: 
+                print("\tAdding convenience variables...")
+                
+            # make the variables more accessible
+            for key in self.array[0]['actuator_disk'].keys(): 
+                self.__dict__[key] = self.array[0]['actuator_disk'][key]
+        
+        if self.verbose: 
+            print("TurbineArray: Initialized from", turb_dir)
+        
+
+if __name__ == "__main__": 
+    """
+    TODO - add unit tests to class
+    """
+    print("turbineArray.py: No unit tests included yet. ")
+    
