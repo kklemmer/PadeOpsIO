836e5bde4a81e5ee650c12206421076933292b83
Kerry Klemmer <kklemmer@mit.edu>
2023-02-01 14:20:18 -0500

    adding budget 4 for time budgets

--- padeopsIO/budgetkey.py
+++ padeopsIO/budgetkey.py
@@ -134,7 +134,162 @@ def get_key():
         'TKE_buoyancy': (3, 7), 
         'TKE_coriolis': (3, 8), 
         #'TKE_AD': (3, 9), 
-        # BUDGET 4 TERMS: TODO
+        # BUDGET 4 TERMS: Reynolds stress budgets
+        'uu_shear_production':              (4,1),
+        'uu_adv':                           (4,2),
+        'uu_turb_transport':                (4,3),
+        'uu_pressure_strain':               (4,4),
+        'uu_pressure_transport':            (4,5),
+        'uu_SGS_transport':                 (4,6),
+        'uu_dissipation':                   (4,7),
+        'uu_buoyancy':                      (4,8),
+        'uu_coriolis':                      (4,9),
+        'uu_AD':                            (4,10),
+        'vv_shear_production':              (4,11),
+        'vv_adv':                           (4,12),
+        'vv_turb_transport':                (4,13),
+        'vv_pressure_strain':               (4,14),
+        'vv_pressure_transport':            (4,15),
+        'vv_SGS_transport':                 (4,16),
+        'vv_dissipation':                   (4,17),
+        'vv_buoyancy': (4,18),
+        'vv_coriolis': (4,19),
+        'vv_AD': (4,20),
+        'ww_shear_production': (4,21),
+        'ww_adv': (4,22),
+        'ww_turb_transport': (4,23),
+        'ww_pressure_strain': (4,24),
+        'ww_pressure_transport': (4,25),
+        'ww_SGS_transport': (4,26),
+        'ww_dissipation': (4,27),
+        'ww_buoyancy': (4,28),
+        'ww_coriolis': (4,29),
+        'ww_AD': (4,30),
+        'uw_shear_production': (4,31),
+        'uw_adv': (4,32),
+        'uw_turb_transport': (4,33),
+        'uw_pressure_strain': (4,34),
+        'uw_pressure_transport': (4,35),
+        'uw_SGS_transport': (4,36),
+        'uw_dissipation': (4,37),
+        'uw_buoyancy': (4,38),
+        'uw_coriolis': (4,39),
+        'uw_AD': (4,40),
+        'vw_shear_production': (4,41),
+        'vw_adv': (4,42),
+        'vw_turb_transport': (4,43),
+        'vw_pressure_strain': (4,44),
+        'vw_pressure_transport': (4,45),
+        'vw_SGS_transport': (4,46),
+        'vw_dissipation': (4,47),
+        'vw_buoyancy': (4,48),
+        'vw_coriolis': (4,49),
+        'vw_AD': (4,50),
+        # BUDGET 5 TERMS: Wake deficit
+        'uwake': (5, 1), 
+        'vwake': (5, 2), 
+        'wwake': (5, 3)
+        }
+    
+    return bidict(key)
+
+
+def get_key_xy(): 
+    """
+    Returns a bidirectional hash table between colloquial string values for budget terms and 
+    a tuple-look up ordered (budget #, term #) as defined in e.g. budget_time_avg.F90. 
+    """
+    key = {  # BUDGET 0 TERMS: (1st and second order averages, scalars excluded)
+        'ubar': (0, 1), 
+        'vbar': (0, 2), 
+        'Tbar': (0, 3), 
+        'uu': (0, 4), 
+        'uv': (0, 5), 
+        'uw': (0, 6), 
+        'vv': (0, 7), 
+        'vw': (0, 8), 
+        'ww': (0, 9), 
+        'uT': (0, 10), 
+        'vT': (0, 11), 
+        'wT': (0, 12), 
+        'TT': (0, 13), 
+        'tau13': (0, 14), 
+        'tau23': (0, 15), 
+        'q3': (0, 16), 
+        'pbar': (0, 17), 
+        'tau11': (0, 18), 
+        'tau12': (0, 19), 
+        'tau22': (0, 20), 
+        'tau33': (0, 21), 
+        # BUDGET 1 TERMS: (momentum)
+        'DuDt': (1, 1),  # x-advection
+        'xSGS': (1, 2),  # x-pressure gradient
+        'xVisc': (1, 3),  # x-sub grid stresses
+        'xCor': (1, 4),   # x-Actuator disk
+        'xGeo': (1, 5),  
+        'xAD': (1, 6), 
+        'DvDt': (1, 7), 
+        'ySGS': (1, 8), 
+        'yVisc': (1, 9), 
+        'yCor': (1, 10),
+        'yGeo': (1, 11), # x-coriolis
+        'DwDt': (1, 12), # x-geostrophic pressure grad. 
+        'zSGS': (1, 13), 
+        'dpdz': (1, 14), 
+        # BUDGET 2 TERMS: (MKE)  TODO - improve the naming keys
+        'MKE_TKE_loss': (2, 1), 
+        'MKE_adv': (2, 2), 
+        'MKE_dissipation': (2, 3), 
+        'MKE_SGS_tau_transport': (2, 4), 
+        'MKE_AD': (2, 5), 
+        'MKE_geostrophic': (2, 6), 
+        'MKE_coriolis': (2, 7),
+        # BUDGET 3 TERMS: (TKE)
+        'TKE_shear_production': (3, 1),
+        'DkDt': (3, 2), 
+        'TKE_p_transport': (3, 3), 
+        'TKE_SGS_transport': (3, 4), 
+        'TKE_dissipation': (3, 5), 
+        'TKE_AD': (3, 6), 
+        'TKE_coriolis': (3, 7), 
+        'TKE_buoyancy': (3, 8), 
+        # BUDGET 4 TERMS:
+        'uu_shear_production': (4, 1), # uu
+        'uu_turb_transport': (4, 2),
+        'uu_p_strain': (4, 3),
+        'uu_p_transport': (4, 4),
+        'uu_SGS_transport': (4, 5),
+        'uu_dissipation': (4, 6),
+        'uu_buoyancy': (4, 7),
+        'uu_coriolis': (4, 8),
+        'uu_AD': (4, 9),
+        'uw_shear_production': (4, 10), # uw
+        'uw_turb_transport': (4, 11),
+        'uw_p_strain': (4, 12),
+        'uw_p_transport': (4, 13),
+        'uw_SGS_transport': (4, 14),
+        'uw_dissipation': (4,15),
+        'uw_buoyancy': (4, 16),
+        'uw_coriolis': (4, 17),
+        'uw_AD': (4, 18),
+        'vw_shear_production': (4, 19), # vw
+        'vw_turb_transport': (4, 20),
+        'vw_p_strain': (4, 21),
+        'vw_p_transport': (4, 22),
+        'vw_SGS_transport': (4, 23),
+        'vw_dissipation': (4, 24),
+        'vw_buoyancy': (4, 25),
+        'vw_coriolis': (4, 26),
+        'vw_AD': (4, 27),
+        'ww_shear_production': (4, 28), #ww
+        'ww_turb_transport': (4, 29),
+        'ww_p_strain': (4, 30),
+        'ww_p_transport': (4, 31),
+        'ww_SGS_transport': (4, 32),
+        'ww_dissipation': (4, 33),
+        'ww_buoyancy': (4, 34),
+        'ww_coriolis': (4, 35),
+        'ww_AD': (4, 36),
         # BUDGET 5 TERMS: Wake deficit
         'uwake': (5, 1), 
         'vwake': (5, 2), 
@@ -231,5 +386,3 @@ def key_labels():
         }
     
     return key
-
-    
