f9bb173267c18eef908bd2f38aa7a968da1645ca
KirbyH <tg877441@login2.stampede2.tacc.utexas.edu>
2023-04-30 20:24:00 -0500

    added new partial derivative ops and vorticity budgets

--- padeopsIO/wake_utils.py
+++ padeopsIO/wake_utils.py
@@ -420,8 +420,373 @@ def partialz2(field, dz):
     return dfdz
 
 
+# Numpy partial derivative functions and index notation help: 
+def e_ijk(i, j, k): 
+    """
+    Permutation operator, takes i, j, k in [1, 2, 3] 
+    
+    returns +1 if even permutation, -1 if odd permutation
+    
+    TODO: This is not elegant
+    """
+    
+    if (i, j, k) in [(0, 1, 2), (1, 2, 0), (2, 0, 1)]: 
+        return 1
+    elif (i, j, k) in [(0, 2, 1), (1, 0, 2), (2, 1, 0)]: 
+        return -1
+    else: 
+        return 0
+    
+
+def d_ij(i, j): 
+    """
+    Kronecker delta
+    """
+    return int(i==j)
+
+
+def ddxi(f, i, dxi=1, edge_order=2): 
+    """
+    Computes partials for a field on an evenly spaced grid in the xi 
+    direction along the i axis (expects 0, 1, 2). 
+    
+    Arguments
+    ---------
+    f (array-like) : field with at least i dimensions
+    i (int) : integer to differentiate along  (subtracts 1 for python)
+    dxi (float) : normalization factor, default 1
+    
+    Returns df/dxi
+    """
+    
+    return np.gradient(f, axis=i, edge_order=edge_order) / dxi
+
+
+# ============= Vorticity computation functions ================
+
+def compute_vort(sl_dict, save_ui=True, save_duidxj=True): 
+    """
+    Computes the vorticity vector and adds `wx`, `wy`, and `wz` the keys field. 
+    
+    Parameters 
+    ----------
+    sl_dict (dict) : slice from BudgetIO.slice()
+    save_duidxj (bool) : saves intermediate key `duidxj` as a 5D tensor in the dictionary, but 
+        does not append to the keys field. Default True. 
+        
+    Returns
+    -------
+    None
+    """
+    
+    if 'duidxj' in sl_dict.keys(): 
+        duidxj = sl_dict['duidxj']
+    else: 
+        duidxj = compute_duidxj(sl_dict)
+    
+    wijk = np.zeros(duidxj.shape + (3, ))  # vorticity component tensor
+    for i in range(3): 
+        for j in range(3): 
+            for k in range(3): 
+                eijk = e_ijk(i, j, k) 
+                if eijk == 0: 
+                    continue
+                wijk[:,:,:,i,j,k] = eijk*duidxj[:,:,:,k,j]
+                
+    if save_duidxj: 
+        sl_dict['duidxj'] = duidxj
+    
+    wi = np.zeros(sl_dict['ubar'].shape + (3, ))
+    vort_keys = ['wx', 'wy', 'wz']
+    for i, wi_key in zip(range(3), vort_keys): 
+        wi[:,:,:,i] = np.sum(wijk[:, :, :, i, :, :], axis=(-2, -1))
+        sl_dict[wi_key] = wi[:,:,:,i]
+        sl_dict['keys'].append(wi)
+    
+    sl_dict['wi'] = wi  # save this for further index notation computations
+    
+    
+def compute_duidxj(sl_dict, save_ui=True): 
+    """
+    Computes partial derivatives in x, y, and z for keys 'ubar', 'vbar', 'wbar'. Assumes the the
+    slice is 3D. 
+    
+    Parameters
+    ----------
+    sl_dict (dict) : dictionary from BudgetIO.slice()
+    ret (bool) : if True, returns the computed quantities instead of appending them in the same dictionary
+    
+    Returns
+    -------
+    duidxj (array-like) : 5D duidxj tensor, indexed [x, y, z, i, j]
+    """
+    
+    ui = assemble_u_tensor(sl_dict)
+    if save_ui: 
+        sl_dict['ui'] = ui
+    
+    x = sl_dict['x']
+    y = sl_dict['y']
+    z = sl_dict['z']
+    dx = [x[1]-x[0], 
+          y[1]-y[0], 
+          z[1]-z[0]]
+
+    ret = np.zeros((len(x), len(y), len(z), 3, 3))
+    for i in range(3): 
+        for j in range(3): 
+            ret[:,:,:,i,j] = ddxi(ui[:,:,:,i], j, dxi=dx[j])
+            
+    return ret
+
+
+def assemble_sgs_tensor(sl_dict): 
+    """
+    Reorganizes the SGS tensor terms 'tau11' etc. into a 5D tensor [x, y, z, i, j] from a 3D slice. 
+    """
+    nx = len(sl_dict['x'])
+    ny = len(sl_dict['y'])
+    nz = len(sl_dict['z'])
+    
+    tau_ij = np.zeros((nx, ny, nz, 3, 3))  # subgrid stresses
+    terms_sgs = [  # matching from budgetkey.py
+        ['tau11', 'tau12', 'tau13'], 
+        ['tau12', 'tau22', 'tau23'], 
+        ['tau13', 'tau23', 'tau33']
+    ]
+
+    # rearrange tensor
+    for ii in range(3): 
+        for jj in range(3): 
+            tau_ij[:,:,:,ii,jj] = sl_dict[terms_sgs[ii][jj]]
+            
+    return tau_ij
+
+
+def assemble_rs_tensor(sl_dict): 
+    """
+    Reorganizes the reynolds stress tensor terms 'uu' etc. into a 5D tensor [x, y, z, i, j] from a 3D slice. 
+    """
+    nx = len(sl_dict['x'])
+    ny = len(sl_dict['y'])
+    nz = len(sl_dict['z'])
+    
+    uiuj = np.zeros((nx, ny, nz, 3, 3))  # reynolds stresses
+    terms_uiuj = [  # matching from budgetkey.py
+        ['uu', 'uv', 'uw'], 
+        ['uv', 'vv', 'vw'], 
+        ['uw', 'vw', 'ww']
+    ]
+
+    # rearrange tensor
+    for ii in range(3): 
+        for jj in range(3): 
+            uiuj[:, :, :, ii, jj] = sl_dict[terms_uiuj[ii][jj]] 
+            
+    return uiuj
+
+
+def assemble_u_tensor(sl_dict): 
+    """
+    Reorganizes the velocity vector into a 4D tensor [x, y, z, i] for component ui
+    """
+    nx = len(sl_dict['x'])
+    ny = len(sl_dict['y'])
+    nz = len(sl_dict['z'])
+    
+    ui = np.zeros((nx, ny, nz, 3)) 
+    terms_ui = ['ubar', 'vbar', 'wbar']  # matching from budgetkey.py
+
+    # rearrange tensor
+    for i in range(3): 
+        ui[:,:,:,i] = sl_dict[terms_ui[i]]
+            
+    return ui
+
+def assemble_w_tensor(sl_dict): 
+    """
+    Reorganizes the vorticity vector into a 4D tensor [x, y, z, i] for component ui
+    """
+    nx = len(sl_dict['x'])
+    ny = len(sl_dict['y'])
+    nz = len(sl_dict['z'])
+    
+    wi = np.zeros((nx, ny, nz, 3)) 
+    terms_wi = ['wx', 'wy', 'wz']  # these terms might not exist and may need to be computed
+
+    if not np.all([term in sl_dict.keys() for term in terms_wi]): 
+        compute_vort(sl_dict, save_ui=True, save_duidxj=True)
+    
+    # rearrange tensor
+    for i in range(3): 
+        wi[:,:,:,i] = sl_dict[terms_wi[i]]
+            
+    return wi
+
+# ============= offline budgets: vorticity ================
+
+def compute_vort_budget(sl_dict, self=None, Ro=None, Ro_f=None, lat=45, Fr=None, theta0=300): 
+    """
+    Computes the offline vorticity budget in three component directions including the terms: 
+        advection '-vort_adv'
+        stretching 'vort_str'
+        buoyancy 'vort_buoy'
+        coriolis 'vort_cor'
+        subgrid stresses 'vort_sgs'
+        Reynolds stresses 'vort_rs'
+        
+    All terms are 4D tensors [x,y,z,i] for the i-direction of vorticity. 
+    
+    Parameters
+    ----------
+    sl_dict (dict) : from BudgetIO.slice(), expects velocity, temperature, subgrid stresses, and reynolds stresses
+    Ro (float) : Rossby number as defined in LES
+    Ro_f (float) : Rossby number as defined Ro = G/(f_c * L_c), where f_c = 2*Omega*sin(lat) is the Coriolis 
+        parmeter. Optional, supersedes Ro if both are given
+    lat (float) : Latitude in degrees, default is 45, NOT None. 
+    Fr (float) : Froude number, defined Fr = G/sqrt(g*L_c)
+    theta0 (float) : Reference potential temperature, Default 300 [K]. 
+    self (BudgetIO object) : optional, if given, gleans Ro_f, Fr, and theta_0 from the following fields in
+        the inputfile: 'ro', 'latitude', 'fr', 'tref'. Supersedes giving parameters individually; default None.  
+    """
+    x = sl_dict['x']; y = sl_dict['y']; z = sl_dict['z']
+    nx = len(x); ny = len(y); nz = len(z)
+    dxi = [x[1]-x[0], y[1]-y[0], z[1]-z[0]]  
+    
+    # allocate memory for all the tensors
+    adv_ij = np.zeros((nx, ny, nz, 3, 3))
+    str_ij = np.zeros((nx, ny, nz, 3, 3))
+    buoy_ij = np.zeros((nx, ny, nz, 3, 3))
+    sgs_ijkm = np.zeros((nx, ny, nz, 3, 3, 3, 3))  # 7D tensor, ugh
+    cor_i = np.zeros((nx, ny, nz, 3))
+    rs_ijkm = np.zeros((nx, ny, nz, 3, 3, 3, 3))  # also 7D
+
+    if self is not None: 
+        Ro_LES = padeopsIO.key_search_r(self.input_nml, 'ro')
+        lat = padeopsIO.key_search_r(self.input_nml, 'latitude') * np.pi/180
+        Ro = Ro_LES/(2*np.sin(lat))  # we need this normalization
+        Fr = padeopsIO.key_search_r(self.input_nml, 'fr')
+        theta0 = padeopsIO.key_search_r(self.input_nml, 'tref')
+    elif Ro_f is not None: 
+        Ro = Ro_f
+    else: 
+        Ro = Ro / (2*np.sin(lat*np.pi/180))  # convert Ro_LES -> Ro_f = G/(f_c*L_c)
+
+    # check all required tensors exist:  (may throw KeyError)
+    if 'ui' in sl_dict.keys(): 
+        u_i = sl_dict['ui']
+    else: 
+        u_i = assemble_u_tensor(sl_dict)
+    
+    if 'wi' in sl_dict.keys(): 
+        w_i = sl_dict['wi']
+    else: 
+        w_i = assemble_w_tensor(sl_dict)
+        
+    if 'uiuj' in sl_dict.keys(): 
+        uiuj = sl_dict('uiuj')
+    else: 
+        uiuj = assemble_rs_tensor(sl_dict)
+        
+    if 'tau_ij' in sl_dict.keys(): 
+        tau_ij = sl_dict['tau_ij']
+    else: 
+        tau_ij = assemble_sgs_tensor(sl_dict)
+        
+    Tbar = sl_dict['Tbar']
+    
+    # compute tensor quantities: 
+    for ii in range(3): 
+        # compute coriolis
+        cor_i[:, :, :, ii] = 1/Ro * ddxi(u_i[:, :, :, ii], 2, dxi=dxi[2])
+
+        for jj in range(3): 
+            # advection (on RHS, flipped sign)
+            adv_ij[:,:,:,ii,jj] = -u_i[:,:,:,jj] * ddxi(w_i[:,:,:,ii], jj, dxi=dxi[jj])
+
+            # vortex stretching
+            str_ij[:,:,:,ii,jj] = w_i[:,:,:,jj] * ddxi(u_i[:,:,:,ii], jj, dxi=dxi[jj])
+
+            # buoyancy torque 
+            eijk = e_ijk(ii, jj, 2)  # buoyancy term has k=3
+            if eijk == 0: 
+                buoy_ij[:,:,:,ii,jj] = 0  # save a bit of compute time by skipping these
+            else: 
+                buoy_ij[:,:,:,ii,jj] = eijk * ddxi(Tbar, jj, dxi=dxi[jj]) / (Fr**2 * theta0)
+
+            for kk in range(3): 
+                # nothing is ijk at the moment, Coriolis w/o trad. approx. is, however
+
+                for mm in range(3): 
+                    # compute permutation operator
+                    eijk = e_ijk(ii, jj, kk)
+
+                    if eijk == 0: 
+                        sgs_ijkm[:,:,:,ii,jj,kk,mm] = 0
+                        rs_ijkm[:,:,:,ii,jj,kk,mm] = 0
+                    else: 
+                        sgs_ijkm[:,:,:,ii,jj,kk,mm] = eijk * ddxi(
+                            ddxi(-tau_ij[:,:,:,kk,mm], mm, dxi=dxi[mm]), jj, dxi=dxi[jj])
+                        rs_ijkm[:,:,:,ii,jj,kk,mm] = eijk * ddxi(
+                            ddxi(-uiuj[:,:,:,kk,mm], mm, dxi=dxi[mm]), jj, dxi=dxi[jj])
+                        
+    # now sum over extra axes to collapse terms
+    vort_raw = {
+        '-vort_adv': adv_ij, 
+        'vort_str': str_ij,
+        'vort_buoy': buoy_ij, 
+        'vort_sgs': sgs_ijkm, 
+        'vort_cor': cor_i, 
+        'vort_rs': rs_ijkm
+    }
+
+    for key in vort_raw.keys(): 
+        dims = len(vort_raw[key].shape)
+        if dims == 4:  
+            sl_dict[key] = vort_raw[key]
+        else: 
+            sum_axes = tuple(range(4, dims))
+            sl_dict[key] = np.sum(vort_raw[key], axis=sum_axes)
+#         sl_dict['keys'].append(key)  # technically I suppose these should not be appended because they are 4D tensors
+
+    # add the residual as well
+    sl_dict['vort_res'] = np.sum([sl_dict[key] for key in vort_raw.keys()], axis=0)
+#     sl_dict['keys'].append('vort_res')
+
+# ============= polar coordinate functions ================
+
+def compute_vort_xrt(sl_dict): 
+    """
+    Transforms the vorticity vector x,y,z -> x,r,theta
+    """
+    
+    if not np.all([keycheck in sl_dict.keys() for keycheck in ['wx', 'wy', 'wz']]): 
+        compute_vort(sl_dict)
+        
+    yy, zz = np.meshgrid(sl_dict['y'], sl_dict['z'], indexing='ij')
+    theta = np.arctan2(zz, yy)
+    # append third axis for explicit broadcasting: 
+    theta = theta[None, :, :]
+
+    sl_dict.update({
+        'wr': sl_dict['wy']*np.cos(theta) + sl_dict['wz']*np.sin(theta), 
+        'wt' : -sl_dict['wy']*np.sin(theta) + sl_dict['wz']*np.cos(theta), 
+        'theta': theta
+    })
+
+    # also compute ur, utheta: 
+    sl_dict.update({
+        'ur': sl_dict['vbar']*np.cos(theta) + sl_dict['wbar']*np.sin(theta), 
+        'ut' : -sl_dict['vbar']*np.sin(theta) + sl_dict['wbar']*np.cos(theta)
+    })
+
+
+# ============= deprecated functions ================
+
 def compute_partials(sl_dict, keys, ret=False): 
     """
+    === DEPRECATED FUNCTION ===
+    
     Computes partial derivatives in x, y, and z for 3D scalar field in `keys`
     
     Parameters
@@ -463,8 +828,10 @@ def compute_partials(sl_dict, keys, ret=False):
         })
     
             
-def compute_vort(sl_dict): 
+def compute_vort_old(sl_dict): 
     """
+    === DEPRECATED FUNCTION ===
+    
     Computes the vorticity vector
     """
     # make sure the partials have already been computed
@@ -483,31 +850,5 @@ def compute_vort(sl_dict):
     })
     
     
-# ============= polar coordinate functions ================
-
-def compute_vort_xrt(sl_dict): 
-    """
-    Transforms the vorticity vector x,y,z -> x,r,theta
-    """
-    
-    if not np.all([keycheck in sl_dict.keys() for keycheck in ['wx', 'wy', 'wz']]): 
-        compute_vort(sl_dict)
-        
-    yy, zz = np.meshgrid(sl_dict['y'], sl_dict['z'], indexing='ij')
-    theta = np.arctan2(zz, yy)
-    # append third axis for explicit broadcasting: 
-    theta = theta[None, :, :]
-
-    sl_dict.update({
-        'wr': sl_dict['wy']*np.cos(theta) + sl_dict['wz']*np.sin(theta), 
-        'wt' : -sl_dict['wy']*np.sin(theta) + sl_dict['wz']*np.cos(theta), 
-        'theta': theta
-    })
-
-    # also compute ur, utheta: 
-    sl_dict.update({
-        'ur': sl_dict['vbar']*np.cos(theta) + sl_dict['wbar']*np.sin(theta), 
-        'ut' : -sl_dict['vbar']*np.sin(theta) + sl_dict['wbar']*np.cos(theta)
-    })
 
 
