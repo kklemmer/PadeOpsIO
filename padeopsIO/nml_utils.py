b061178133e78815f0be52c2c15e515978797b5a
Kirby <kirby.heck@gmail.com>
2023-07-10 11:04:03 -0400

    cleaned namelist parsing

--- padeopsIO/nml_utils.py
+++ padeopsIO/nml_utils.py
@@ -8,7 +8,8 @@ def parser(filename):
 
     Parameters 
     ----------
-    filename (path-like) : path to a namelist text file
+    filename : path
+        Path to a namelist text file
 
     Returns
     -------
@@ -20,36 +21,32 @@ def parser(filename):
     Active = None
     with open(filename, 'r') as f: 
         lines = f.readlines()
-
+        
+        # loop through lines
         for line in lines: 
-            res = re.search('&(\S+)', line)
-            if res is not None: 
-                # print('Namelist name: ', res.group(1))
-                Active = res.group(1)  # active list
+            res = re.search('&(\S+)', line)  # namelists start with "&"
+            if res is not None:  # starting new namelist
+                Active = res.group(1)  # save string in active list
                 continue
-
+            
+            # within a namelist now; search `variable` = `value`
             res = re.search('(\S+)\s+=\s+(\S+)', line)
             if res is not None: 
-                # tmp = re.sub('[dD]', 'e', res.group(2))
+                # found variable/value pairing
                 key = res.group(1)
                 value = cast_str_to_X(res.group(2))
-                # try: 
-                #     value = float(tmp)
-                # except ValueError as e: 
-                #     value = res.group(2)
-                # TODO: Handle boolean variables too
+                # TODO: Handle boolean variables too? 
 
-                # print(key, value)
                 Namelist[key] = value
                 continue
 
             res = re.search(r'/', line)
             if res is not None: 
-                # print('Found end of Namelist')
+                # Found end of Namelist
                 if Active is not None: 
                     Namelists[Active] = Namelist.copy()
                     Namelist = {}
-                    Active = None
+                    Active = None  # terminate namelist, start a new one
 
     return Namelists
 
@@ -57,34 +54,43 @@ def writer(filepath, Namelists):
     """
     Writes a namelist formatted in the same way that is output by nml_parser()
 
+    Parameters
+    ----------
+    filepath : path
+        Path where the namelist will be written
+    Namelists : Nested dict from parser()
+
+    Returns
+    -------
+    None
     """
-    with open(filepath, 'w') as f: 
+    with open(filepath, 'w') as f:  # begin writing a file
         for key in Namelists.keys(): 
+            # write header
             f.write('&' + key + '\n')
+            # loop thru variables
             for var in Namelists[key].keys(): 
                 tmp = Namelists[key][var]
                 value = cast_to_str(tmp)
+                # write variable/value combinations
                 f.write('{:''<24} = {:s}\n'.format(var, value))
             f.write('/\n')
 
 
-def isfloat(value): 
-    """
-    Checks if the input can be cast into a float. 
-    https://www.programiz.com/python-programming/examples/check-string-number
-
-    Deprecated 06/07/2023
-    """
-    try: 
-        float(value)
-        return True
-    except ValueError: 
-        return False
-
-
 def cast_str_to_X(value): 
     """
-    Tries to cast the input value to an integer, then a float. If both, then returns the original string. 
+    Tries to cast the input value to an integer, then a float. 
+    If all fail, then returns the original string. 
+
+    Parameters
+    ----------
+    value : str
+        Value read in by the parser as a string
+    
+    Returns
+    -------
+    value : str or int or float
+        Cast value
     """
 
     # try integer casting: 
@@ -100,12 +106,32 @@ def cast_str_to_X(value):
         tmp2 = float(tmp1)
         return tmp2
     except ValueError: 
-        return value  # returns original value
+        pass
+
+    # TODO: add boolean 
+
+    return value  # returns original value
     
 
 def cast_to_str(value): 
     """
     Takes a value of unknown type (value) and casts to a fortran namelist-readable string. 
+
+    Parameters
+    ----------
+    value : str or float or int
+        Variable value to write into the namelist
+    
+    Returns
+    -------
+    ret : str
+        String representation of the value. 
+        For example: 
+            int(5)      ->  '5'
+            'abc'       ->  'abc'
+            float(5)    ->  '5.00000000e+00'  (single precision)
+            # TODO: add booleans: 
+            bool(True)  ->  '.true.'
     """
 
     # note: this is NOT a try/except statement because we want floats (that are also integers)
